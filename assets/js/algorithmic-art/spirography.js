d2947b63d5d20298d8689999b96ed842713aff4c
vw201cv <ugurkostak@hotmail.com>
2026-06-02 19:08:16 +0200

    cinema: add Spirography visualization and metadata

---
+++ assets/js/algorithmic-art/spirography.js
@@ -0,0 +1,11 @@
+/**
+ * Spirography Visualization
+ * TODO: Implement spirography pattern generation using hypotrochoid/epitrochoid curves
+ */
+
+// TODO: Initialize canvas and rendering context
+// TODO: Set up parameter controls (outer radius, inner radius, pen distance)
+// TODO: Implement parametric curve generation
+// TODO: Handle canvas resizing and responsive behavior
+// TODO: Add user interactions (drag, parameter adjustment)
+// TODO: Optimize rendering performance for smooth animations
